4bfd54e67bf54429072aac1c6875e9d4cff67a1c
Sebastian Ricaurte <1jr@hotmail.com>
2022-09-21 08:49:49 -0500

    apuntos 2, java poo

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,20 +1,24 @@
 public class Doctor {
     //atributos
-    int id;
+    //int id; // autoincrementado
+    static int id = 0;
     String name;
     String speciality;
-
     Doctor(){
         System.out.println("construyendo el objeto doctor");
+        id++;
     }
-
     Doctor(String name){
         System.out.println("El nombre del Doctor asignado es: " + name);
     }
-
     //comportamientos
     //metodo
     public void showName(){
         System.out.println(name);
     }
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
+    }
+
+
 }
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,9 +1,19 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-
-        Doctor myDoctor = new Doctor("Anahi Salgado");
+        /*
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Sebastian Ricaurte";
         myDoctor.showName();
+        myDoctor.showId();
 
+        Doctor MyDoctorAnn = new Doctor();
+        myDoctor.showId();
+        //metodo
+        */
+        showMenu();
     }
+
+
 }
